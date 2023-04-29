d85cc691413bbde0600de8d044b3ac533ccb4ece
Vladimir-str2207 <andrianovkdv@mail.ru>
2023-04-29 22:29:24 +0500

    CatPic commit

---
+++ CatPic.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import random
+
+
+def get_images():
+    cats = ['кот', 'кошка', 'котята']
+    url = f'https://fonwall.ru/search?q={random.choice(cats)}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    pictures = soup.find_all('img', class_='photo-item__img')
+    clear_pictures = [item['src'] for item in pictures]
+    return random.choice(clear_pictures)
+
+
+print(get_images())
