62e59cee7d5c9c4613290c6d9df3ee5056a25aca
akosi <akosishnov@mail.ru>
2023-05-03 21:10:53 +0500

    added chng_tf-func

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import logging
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import Message
 from aiogram.utils import executor
+from aiogram.utils.callback_data import CallbackData
 
 from exchanges import all_ex_price
 from CatPic import get_images
@@ -21,39 +24,95 @@ total_keyboard.add(types.InlineKeyboardButton(text='Price', callback_data='price
 total_keyboard.add(types.InlineKeyboardButton(text='Longs&Shorts', callback_data='ratio'))
 total_keyboard.add(types.InlineKeyboardButton(text='Сat', callback_data='cat'))
 
+timeframes_keyboard = types.InlineKeyboardMarkup(row_width=2)
+cb = CallbackData('timeframe', 'time', 'figure')
+timeframes_keyboard.add(types.InlineKeyboardButton(text='5m', callback_data=cb.new(time='m', figure='5')))
+timeframes_keyboard.add(types.InlineKeyboardButton(text='30m', callback_data=cb.new(time='m', figure='30')))
+timeframes_keyboard.add(types.InlineKeyboardButton(text='1h', callback_data=cb.new(time='h', figure='1')))
+timeframes_keyboard.add(types.InlineKeyboardButton(text='4h', callback_data=cb.new(time='h', figure='4')))
+timeframes_keyboard.add(types.InlineKeyboardButton(text='24h', callback_data=cb.new(time='h', figure='24')))
+timeframes_keyboard.add(types.InlineKeyboardButton(text='< Назад', callback_data='back'))
+
+user_data = {}
+
+
+class ChooseTimeframe(StatesGroup):
+    choosing_timeframe = State()
+
 
 @dp.message_handler(commands=['start'])
-async def start(message):
+async def start(message: types.Message | types.CallbackQuery):
     """
     This handler will be called when user sends `/start` or `/help` command
+    Creates standard timeframe and coin for current user
     """
-
-    await message.reply("Привет, этот бот подскажет тебе курсы по интересующей тебя монете\n"
-                        "И соотношение лонгистов и шортистов\n"
-                        "Для управления используй команды из блока /help", reply_markup=total_keyboard)
+    user_id = message.from_user.id
+    if user_id not in user_data:
+        data = {'timeframe': 'h1',
+                'coin': 'BTC'}
+        user_data[user_id] = data
+    if isinstance(message, types.Message):
+        await message.answer("Привет, этот бот подскажет тебе курсы по интересующей тебя монете\n"
+                             "И соотношение лонгистов и шортистов\n"
+                             "Для управления используй команды из блока /help", reply_markup=total_keyboard)
+    elif isinstance(message, types.CallbackQuery):
+        await message.message.edit_text("Привет, этот бот подскажет тебе курсы по интересующей тебя монете\n"
+                                        "И соотношение лонгистов и шортистов\n"
+                                        "Для управления используй команды из блока /help", reply_markup=total_keyboard)
 
 
 @dp.message_handler(commands=['help'])
 async def help(message):
     """Отправляюет пользователю описание команд"""
-    await message.reply('Бот имеет следующие команды:\n/price - выводит список цен на 7 различных биржах\n'
-                        '/ratio - выводит соотношение лонгистов и шортистов на BTC\n'
-                        '/cat - интересная функция, позволяющая насладиться преимуществами технологий')
+    await message.answer('Бот имеет следующие команды:\n/price - выводит список цен на 7 различных биржах\n'
+                         '/ratio - выводит соотношение лонгистов и шортистов на BTC\n'
+                         '/cat - интересная функция, позволяющая насладиться преимуществами технологий')
 
 
-async def ratio(message):
+async def ratio(message: types.Message | types.CallbackQuery):
     """Собирает данные с сайта CoinGlass.com и отправляет их пользователю"""
-    timeframe = 'h1'
+    user_id = message.from_user.id
+    data = user_data[user_id]
+    timeframe = data['timeframe']
+    ratio_keyboard = types.InlineKeyboardMarkup(row_width=2)
+    ratio_keyboard.add(types.InlineKeyboardButton(text='Price', callback_data='price'))
+    ratio_keyboard.add(types.InlineKeyboardButton(text='Longs&Shorts', callback_data='ratio'))
+    ratio_keyboard.add(types.InlineKeyboardButton(text='Сat', callback_data='cat'))
+    ratio_keyboard.add(types.InlineKeyboardButton(text='Поменять таймфрейм', callback_data='change_timeframe'))
     cg_data = CoinGlass.get_data(timeframe=timeframe)
-    if cg_data.__class__ != Exception.__class__:
+    if cg_data.__class__ != Exception.__class__ and cg_data != 'data':
         longs, shorts = cg_data
-        mes = f"Позиций лонг: {longs}; позиций шорт: {shorts}"
+        mes = f"Позиций лонг: {longs}; позиций шорт: {shorts}\nТаймрфейм: {timeframe}"
     else:
         mes = f"Небольшие неполадки, попробуйте позднее"
     if isinstance(message, types.Message):
-        await message.reply(mes)
+        await message.answer(mes)
     elif isinstance(message, types.CallbackQuery):
-        await message.message.reply(mes, reply_markup=total_keyboard)
+        await message.message.edit_text(mes, reply_markup=ratio_keyboard)
+
+
+@dp.callback_query_handler(text='change_timeframe')
+async def change_timeframe(message: Message):
+    user_id = message.from_user.id
+    data = user_data[user_id]
+    timeframe = data['timeframe']
+    if isinstance(message, Message):
+        await message.answer(text=f'Текущий таймфрейм: {timeframe}\n' +
+                                  f'Нажмите на кнопку ниже, чтобы изменить', reply_markup=timeframes_keyboard)
+    elif isinstance(message, types.CallbackQuery):
+        await message.message.edit_text(text=f'Текущий таймфрейм: {timeframe}\n' +
+                                             f'Нажмите на кнопку ниже, чтобы изменить',
+                                        reply_markup=timeframes_keyboard)
+
+
+@dp.callback_query_handler(cb.filter())
+async def timeframe_chosen(call: types.CallbackQuery, callback_data: dict):
+    timeframe = callback_data['time'] + callback_data['figure']
+    user_id = call.from_user.id
+    data = user_data[user_id]
+    del data['timeframe']
+    data['timeframe'] = timeframe
+    await change_timeframe(call)
 
 
 @dp.callback_query_handler(text='ratio')
@@ -68,16 +127,19 @@ async def message_ratio(message):
 
 async def price(message: types.CallbackQuery | types.Message):
     """Собирает данные о цене с топ 7 бирж и отправляет их пользователю в отсортированном порядке."""
-    total = all_ex_price()
+    user_id = message.from_user.id
+    data = user_data[user_id]
+    coin = data['coin']
+    total = all_ex_price(coin=coin)
     mes = f'Цены на биржах:\n'
     all_prices = sorted([(key, value) for key, value in total.items()], key=lambda x: x[1])
     for ex in range(len(all_prices)):
         mes = mes + str((ex + 1)) + ") " + all_prices[ex][0].capitalize() + ": " + str(all_prices[ex][1]) + "\n\n"
     mes = mes.rstrip()
     if isinstance(message, types.Message):
-        await message.reply(mes)
+        await message.answer(mes)
     elif isinstance(message, types.CallbackQuery):
-        await message.message.reply(mes, reply_markup=total_keyboard)
+        await message.message.edit_text(mes, reply_markup=total_keyboard)
 
 
 @dp.callback_query_handler(text='price')
@@ -96,7 +158,7 @@ async def cat(message):
     if isinstance(message, types.Message):
         await message.reply(link)
     elif isinstance(message, types.CallbackQuery):
-        await message.message.reply(link, reply_markup=total_keyboard)
+        await message.message.edit_text(link, reply_markup=total_keyboard)
 
 
 @dp.callback_query_handler(text='cat')
@@ -107,6 +169,11 @@ async def inline_cat(call):
 @dp.message_handler(commands=['cat'])
 async def message_cat(message):
     await cat(message)
+
+
+@dp.callback_query_handler(text='back')
+async def message_cat(call):
+    await start(call)
 
 
 def main():
